a9c69575426b9c2bcd662c00816cad73af3382be
nsstrelets02021996-web <ns.strelets02021996@gmail.com>
2026-05-26 23:39:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ CHAT_ID = "8611901179"
 
 bot = Bot(token=TOKEN)
 
+async def test_message():
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="Бот підключений ✅"
+    )
+
+asyncio.run(test_message())
+
 # =========================
 # TRADINGVIEW
 # =========================
