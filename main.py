aed475e2939fe3371fa3f6175bd51009601159a5
nsstrelets02021996-web <ns.strelets02021996@gmail.com>
2026-05-26 20:16:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import os
 # TELEGRAM
 # =========================
 
-TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+TOKEN = "8817561227:AAHo0m5lRbUod_ytWsIS-BUkNaOlmgH-obo"
+CHAT_ID = "8817561227"
 
 bot = Bot(token=TOKEN)
 
